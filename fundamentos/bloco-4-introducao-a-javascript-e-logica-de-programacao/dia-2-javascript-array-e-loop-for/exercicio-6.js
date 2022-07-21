f49206e0d710ae1d646a86906827f38b852178a1
andre-usf <andrefretta@gmail.com>
2022-07-21 16:07:07 -0300

    Completa exercício 06.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-6.js
@@ -1,2 +1,17 @@
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let contadorImpar = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  if (numbers[index] % 2 !== 0) {
+    contadorImpar += 1
+  }
+}
+
+if (contadorImpar === 0) {
+  console.log("Nenhum valor ímpar encontrado");
+}
+else {
+  console.log("Foram encontrados " + contadorImpar + " números ímpares");
+}
