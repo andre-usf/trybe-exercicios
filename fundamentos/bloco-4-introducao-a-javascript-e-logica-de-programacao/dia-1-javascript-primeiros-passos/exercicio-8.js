663327988735134e7a3f04583975ac568068e646
andre-usf <andrefretta@gmail.com>
2022-07-20 21:32:37 -0300

    Completa o exercício 08.

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-8.js
@@ -0,0 +1,15 @@
+/*
+Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+Bonus: use somente um if.
+*/
+
+const numeroUm = 5;
+const numeroDois = 1;
+const numeroTres = 7;
+
+if (numeroUm % 2 == 0 || numeroDois % 2 == 0 || numeroTres % 2 == 0){
+  console.log("Um dos números é par!")
+}
+else {
+  console.log("Todos os números são ímpares!")
+}
