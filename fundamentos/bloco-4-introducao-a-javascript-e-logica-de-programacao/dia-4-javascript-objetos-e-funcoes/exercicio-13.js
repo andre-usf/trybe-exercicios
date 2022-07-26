0906b6086480faa4f99c9c141c9d590823667892
andre-usf <andrefretta@gmail.com>
2022-07-26 11:21:01 -0300

    Completa o exercício 13.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-13.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicio-13.js
@@ -4,26 +4,28 @@ Array de teste: [2, 3, 2, 5, 8, 2, 3];.
 Valor esperado no retorno da função: 2.
 */
 
-function inteiroMaisRepete (array) {
-let contadorIfNumeroIgual = 0; // Conta quantas vezes o número do index 1 é igual ao do index 2.
+function inteiroQueMaisRepete (array) {
+let contadorVezesRepete = 0; // Conta quantas vezes o número se repete.
+let numMaisSeRepete = 0; // Guarda o número que mais se repete.
+let indexNumMaisSeRepete = 0; // Guarda o index do número que mais se repete.
 
   for (let indexUm in array) {
     
     
     for (let indexDois in array) {
-      
-      
       if (array[indexUm] === array[indexDois]) {
-        contadorIfNumeroIgual += 1;
-        
+        contadorVezesRepete += 1;
       }
-
     }
-
-    
-
+  
+  if (contadorVezesRepete > numMaisSeRepete) {
+    numMaisSeRepete = contadorVezesRepete
+    indexNumMaisSeRepete = indexUm
   }
-
+  
+  contadorVezesRepete = 0;
+  }
+  return array[indexNumMaisSeRepete]
 }
 
-console.log(inteiroMaisRepete([2, 3, 2, 5, 8, 2, 3]));
+console.log(inteiroQueMaisRepete([2, 3, 2, 5, 8, 2, 3]));
