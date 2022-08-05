51264e43816aff72485f7423c2215d7db9084a8a
andre-usf <andrefretta@gmail.com>
2022-08-05 20:57:14 -0300

    Inicia exercício bônus.

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio-do-dia/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/exercicio-do-dia/script.js
@@ -1,8 +1,40 @@
-function preventEnviar(event) {
-  event.preventDefault();
+window.onload = preventEnviar(), apagarCampos(), habilitaEnviar();
+
+function preventEnviar () {
+  const botao = document.querySelector('#botao-enviar');
+  botao.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+};
+
+function apagarCampos() {
+  const botaoApagar = document.querySelector('#botao-apagar');
+  
+  botaoApagar.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('input');
+    const areaTexto = document.querySelector('#porque');
+    
+    for (let index = 0; index < inputs.length; index += 1) {
+      inputs[index].value = '';
+    }
+
+    areaTexto.value = '';
+
+  })
+};
+
+function habilitaEnviar () {
+  const botaoEnviar = document.querySelector('#botao-enviar');
+  const concordancia = document.querySelector('#concordo');
+
+  concordancia.addEventListener('change', () => {
+      botaoEnviar.disabled = !concordancia.checked;
+
+  });
 }
 
-window.onload = function() {
-  const botao = document.querySelector('#botao-enviar');
-  botao.addEventListener('click', preventEnviar);
-}
+function validaCampos() {
+
+
+
+};
