f03d03a6c2d7341cd5f0e5cb07efae86c640b782
andre-usf <andrefretta@gmail.com>
2022-08-23 15:43:03 -0300

    Realiza exercício 02.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio-2.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercicio-2.js
@@ -61,21 +61,47 @@ const books = [
   },
 ];
 
-// Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
+// Construa um array de objetos a partir do array de livros. 
+// Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. 
+// O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
 
-let nomeLivroAutor = [];
+const arrayLivros = books.map((book) => {
+  const idadeLancamento = book.releaseYear - book.author.birthYear;
 
-const pegaInfo = books.map((book) => nomeLivroAutor.push(`${book.name} - ${book.genre} - ${book.author.name}`));
+  return {
+    author: book.author.name,
+    age: idadeLancamento,
+  }
+}).sort((a, b) => a.age - b.age);
 
-console.log(nomeLivroAutor);
+
+console.log(arrayLivros);
 
 /*
 const expectedResult = [
-  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
-  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
-  'Fundação - Ficção Científica - Isaac Asimov',
-  'Duna - Ficção Científica - Frank Herbert',
-  'A Coisa - Terror - Stephen King',
-  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
 ];
 */
