4b11f17f5cf3717cca84a4aa9896098a5c30340f
andre-usf <andrefretta@gmail.com>
2022-08-23 15:10:44 -0300

    Inicia exercício 07.

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-7.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercicio-7.js
@@ -63,9 +63,3 @@ const books = [
 
 // Faça uma função que retorne true, caso nenhum author tenha nascido no mesmo ano, e false, caso contrário.
 
-const resultadoEsperado = false;
-
-const autoresMesmoAno = books.some(() => {
-  
-});
-
